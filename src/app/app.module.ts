c759d27b131cb70c7c1a519a0de77df64b50db8c
srichakra <srichakra.koppula@gmail.com>
2018-02-13 07:49:18 -0500

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import 'hammerjs'
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { DishService } from './services/dish.service';
 
 
 @NgModule({
@@ -27,7 +28,9 @@ import { DishDetailComponent } from './dish-detail/dish-detail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
