565b8b72e15cc7642e0ec8cf111690a434144f60
srichakra <srichakra.koppula@gmail.com>
2018-02-16 00:18:03 -0500

    Single page application - part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -43,7 +44,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
